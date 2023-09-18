085e6fa883e2d0f706d6943e32aa9a074a63a591
Vasyl <vasylmiskiv@gmail.com>
2023-09-18 22:27:23 +0300

    app config has been added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HttpModule } from '@nestjs/axios';
 
@@ -13,36 +12,40 @@ import { ArticlesRepository } from './articles/articles.repository';
 import { Article, ArticleSchema } from './articles/entities/article.schema';
 
 import { AuthModule } from './auth/auth.module';
-import { AuthController } from './auth//auth.controller';
+import { AuthController } from './auth/auth.controller';
 
 import { UserRepository } from './users/user.repository';
-
 import { UsersModule } from './users/users.module';
 import { User, UserSchema } from './users/entities/user.schema';
-import { ConfigService } from './config/config.service';
 
-import configuration from './config/configuration';
+import { AppConfigModule } from './config/config.module';
+import { AppConfigService } from './config/config.service';
 
 @Module({
   imports: [
+    AppConfigModule,
     ArticlesModule,
     AuthModule,
     ScheduleModule.forRoot(),
-    ConfigModule.forRoot({
-      load: [configuration],
-    }),
     HttpModule,
+    MongooseModule.forRootAsync({
+      imports: [AppConfigModule],
+      useFactory: async (appConfigService: AppConfigService) => ({
+        uri: appConfigService.mongodb,
+      }),
+      inject: [AppConfigService],
+    }),
     MongooseModule.forFeature([{ name: Article.name, schema: ArticleSchema }]),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     UsersModule,
   ],
   controllers: [AuthController, ArticlesController],
   providers: [
+    AppConfigService,
     RssScheduleService,
     ArticlesRepository,
     UserRepository,
     ArticlesService,
-    ConfigService,
   ],
 })
 export class AppModule {}
--- src/config/config-validation.ts
+++
@@ -1,9 +0,0 @@
-import * as Joi from 'joi';
-
-export const configValidationSchema = Joi.object({
-  port: Joi.number().integer().default(8080),
-  mongodb: Joi.string().required(),
-  jwtsecret: Joi.string().required(),
-  news: Joi.string().required(),
-  newskey: Joi.string().required(),
-});
